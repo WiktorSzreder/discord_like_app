3389ed1bc0f4588d279cc41398b7d74121d7413b
wiktor <wiktor.szreder@icloud.com>
2025-02-26 10:53:05 +0100

    Add templates to project

--- base/views.py
+++ base/views.py
@@ -1,9 +1,20 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
-    return HttpResponse("Hello world")
 
-def room(request):
-    return HttpResponse("ROOM")
+rooms = [
+    {'id':1, 'name':'Lets learn python!'},
+    {'id':2, 'name':'Design with me'},
+    {'id':3, 'name':'Frontend developers'}
+]
+def home(request):
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context) #context send data on tamplate in this case- context=rooms(list of dictionaries)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html',context)
