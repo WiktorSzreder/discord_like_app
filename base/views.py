06a881d818f3c8b0058c4c8e559cba6a310e51ee
wiktor <wiktor.szreder@icloud.com>
2025-03-09 08:57:02 +0100

    Add chat CRUD

--- base/views.py
+++ base/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 
 
@@ -76,7 +76,21 @@ def home(request):
     return render(request, 'base/home.html', context) #context send data on template in this case - context=rooms(list of dictionaries)
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+
+    participants = room.participants.all()
+
+    if request.method == "POST":
+        message = Message.objects.create(
+            user= request.user,
+            room= room,
+            body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+
+    context = {'room': room, 'room_messages': room_messages, 'participants': participants}
     return render(request, 'base/room.html',context)
 @login_required(login_url='login')
 def createRoom(request):
@@ -114,3 +128,15 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html',{'obj': room})
+
+@login_required(login_url='login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You do not have permission')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html',{'obj': message})
